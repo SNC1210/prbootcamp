05cf79eb8548811f136d8b39525cc6e7fe78d7ca
SNC1210 <47062459+SNC1210@users.noreply.github.com>
2021-03-21 18:42:38 +0530

    Add files via upload

---
+++ largest3number.js
@@ -0,0 +1,27 @@
+//problem 1 of codekata solved in two time
+
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+ 
+ var N1= parseFloat(userInput[0].split(" "));
+ var N2= parseFloat(userInput[1].split(" "));
+ var N3= parseFloat(userInput[2].split(" "));
+  var largest=0;
+  if (N1>=N2 && N1>=N3)
+     largest=N1
+  else if (N2>=N1 && N2>=N3)
+      largest=N2;
+  else
+      largest=N3;
+
+console.log(largest);
+
+});
+ 
